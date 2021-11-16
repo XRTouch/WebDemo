c2f7f0d7ca6140d0e9d42991f56c6f1ee14b2cbe
Quent5 <dalbon3u@etu.univ-lorraine.fr>
2021-11-16 18:15:49 +0100

    Update tu.js
    test de texte

--- tu.js
+++ tu.js
@@ -24,6 +24,28 @@ import * as THREE from "https://cdn.skypack.dev/three@0.134.0";
             camera.position.z = 3;
 
 
+            var fontload = new THREE.FontLoader().load('./fonts/helvetiker_bold.typeface.js', function(fontload) {
+
+                const geometry3 = new THREE.TextGeometry( 'XrTouch', {
+                    font: font, 
+                    size: 80,
+                    height: 5,
+                    curveSegments: 12,
+                    bevelEnabled: true,
+                    bevelThickness: 10,
+                    bevelSize: 8,
+                    bevelOffset: 0,
+                    bevelSegments: 5
+                } );
+            } );
+
+            var textMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000, specular: 0xffffff });
+
+            var mesh = new THREE.Mesh( geometry3, textMaterial );
+
+            scene.add( mesh );
+
+
             function animate(){
                 requestAnimationFrame(animate);
                 renderer.render(scene, camera);
