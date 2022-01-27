e8f1cb246bb4ee94cd03c6b93779e295c27a4a92
FurWaz <fur.waz06@gmail.com>
2022-01-27 15:28:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var http = require('http');
 var express = require('express');
 const app = express();
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const addon = require("./cppInterface/cppInterface");
+console.log('init');
+addon.init("COM5");
 
 /* 
 Serveur web par defaut :)
@@ -14,6 +19,20 @@ app.get('/*', (req, res) => {
     path = path.split("?")[0];
     path = __dirname+"/client"+path;
     res.sendFile(path);
+});
+
+io.on("connection", socket => {
+    let lastValue = -1;
+    setInterval(() => {
+        let newValue = addon.getAngle();
+        if (Math.abs(newValue - lastValue) >= 1) {
+            lastValue = newValue
+            socket.emit("custom/getAngle", lastValue);
+        }
+    }, 33);
+    socket.on("custom/setAngle", val => {
+        addon.setForce(Math.max(Math.min(parseInt(val)+30, 100), 0));
+    });
 });
 
 server.listen(80);
